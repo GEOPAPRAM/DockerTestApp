921c5af9282847ca6671bc22f1bd7bacfdc6d730
George Papadopoulos <geopapram@gmail.com>
2016-01-02 12:03:56 +0000

    finally added unit tests project

---
+++ DockerSelfhostWebApi.Tests/HelloWorldTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace DockerSelfhostWebApi.Tests
+{
+    [TestFixture]
+    public class HelloWorldTests
+    {
+        [Test]
+        public void HelloWorldTest()
+        {
+            var controller = new DockerSelfhostWebApi.DemoController();
+
+            var result = controller.HelloWorld();
+
+            Assert.That(result, Is.StringMatching(@"Hello, cosmos! Said at \d{2}:\d{2}"));
+        }
+    }
+}
